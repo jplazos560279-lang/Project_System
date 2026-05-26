9b92f5de1bc881eee91deceeefebd7cd9f33fc21
jplazos560279-lang <j.plazos.560279@umindanao.edu.ph>
2026-05-26 22:40:52 +0800

    push PayrollCalculator.php

---
+++ PayrollCalculator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class PayrollCalculator
+{
+    /**
+     * Calculate the final net salary.
+     *
+     * @param float $baseSalary
+     * @param float $deductions
+     * @return float
+     */
+    public function calculateNetPay(float $baseSalary, float $deductions): float
+    {
+        return $baseSalary - $deductions;
+    }
+}
